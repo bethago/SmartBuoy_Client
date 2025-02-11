0ee3acbb5aad1c121a3bf0cb16607e5d76431f69
bethago <cashpaper7@gmail.com>
2025-02-11 17:59:30 +0900

    V2.0.2

--- sensors/mock_data.py
+++ sensors/mock_data.py
@@ -3,8 +3,14 @@ import random
 values = {'accelerometer':[0, 0, 0], 'gps':[37.500000, 131.000000, 0.0], 'ultrasonic':[0], 'temperature':[11.0]}
 
 def modify_randomly(values):
-    # return [round(value + random.uniform(-0.1, 0.1), 6) for value in values]
-    return [round(value + random.uniform(-5, 5), 6) for value in values]
+    new_values = []
+    for value in values:
+        # modified_value = round(value + random.uniform(-0.1, 0.1), 6)
+        modified_value = round(value + random.uniform(-5, 5), 6)
+        if modified_value < 0:
+            modified_value = 0
+        new_values.append(modified_value)
+    return new_values
 
 def set_sensors(buoy):
     sensor_datas = buoy.sensors
